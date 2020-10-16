29bbeb6686de4f3fb723ff997e3e3a78c3845744
bd_x <2946578538@qq.com>
2020-10-16 15:38:19 +0800

    servelet1

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import TextInput from './react-lib/components/element/elementTextfiled'
 import InputAdornment from "@material-ui/core/InputAdornment";
 import {AccountCircle} from "@material-ui/icons";
-
+import axiox from 'axios'
 
 class Test extends React.Component{
     constructor(props) {
@@ -13,7 +13,15 @@ class Test extends React.Component{
     }
 
     componentDidMount() {
-
+        let url = 'https://petstore.swagger.io/v2/pet/1'
+        axiox({url:url})
+            .then((res)=>{
+                console.log(res.data)
+                console.log(res.status)
+            })
+            .catch((err)=>{
+                console.log(err.response)
+            })
     }
 
     oneItem = {
